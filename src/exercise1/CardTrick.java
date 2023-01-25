e040c986b5bff75a0eebb5fcb3c92a8690110460
dobariys <dobariys@sheridancollege.ca>
2023-01-25 10:53:26 -0500

    I am done.

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -63,7 +63,7 @@ public class CardTrick {
      * @author Paul Bonenfant Jan 2022
      */
     private static void printInfo() {
-    
+        //i'm done
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
